f8b1a656a70a3ddc0cf3f5da63c429cd01410cc0
k8tems <k8tems@live.com>
2024-03-07 16:19:00 +0900

    Implement oob auth(auth in which you paste the code to the console instead of getting redirected with the code)

--- init.py
+++ init.py
@@ -1,9 +1,10 @@
+import os
 import sys
 from google_auth_oauthlib.flow import Flow
 import refresh
 
 
-redirect_uri = 'http://localhost:8080/'
+redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
 scopes = ['https://www.googleapis.com/auth/drive']
 
 
@@ -20,22 +21,12 @@ def auth(client_id, client_secret):
         scopes=scopes,
         redirect_uri=redirect_uri
     )
-
-    # TODO: コードをコピーして貼り付ける形式にするにはどうすればいい？
-    auth_url, _ = flow.authorization_url(prompt='consent')
-
+    auth_url, _ = flow.authorization_url(prompt='consent', access_type='offline')
     print("Please go to this URL and authorize access:")
     print(auth_url)
-    print("After authorizing, you will be redirected to a URL with a 'code' parameter.")
-    print("Paste the full redirected URL here:")
-
-    redirected_url = input()
-    import os
-
+    code = input('Code: ')
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
-    flow.fetch_token(authorization_response=redirected_url)
-
+    flow.fetch_token(code=code)  # Use the provided code to fetch the token
     print("Access Token:", flow.credentials.token)
     print("Refresh Token:", flow.credentials.refresh_token)
 
